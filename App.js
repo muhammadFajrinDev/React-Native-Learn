644acd4dd21c4a1ebe5faab58080b2a79ff6322e
Mohamad Fazrin Fahlevi <mWX866213@china.huawei.com>
2021-06-02 15:07:54 +0700

    Mengubah text

--- App.js
+++ App.js
@@ -71,7 +71,7 @@ const App: () => Node = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-          <Text>Hello Sayang</Text>
+          <Text>Hello Sayangs</Text>
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
           </Section>
